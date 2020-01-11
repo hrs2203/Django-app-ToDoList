61785465479da3c86d846b5756a2b5a4a5787d8e
Hrishabh Pandey <hrishabh.p18@iiits.in>
2020-01-12 01:24:37 +0530

    Revert "rearranging static file for deployment"
    This reverts commit bf781f6a1ebbdbe4b488cfb62d3b3ef2975ff25f.

--- todoApp/todoApp/settings.py
+++ todoApp/todoApp/settings.py
@@ -159,11 +159,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-# for deploying
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
-# for developement
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static','todo'),
-    os.path.join(BASE_DIR, 'static','mainApp'),
-    os.path.join(BASE_DIR, 'static','sendMail'),
+    os.path.join(BASE_DIR, 'todo', 'static','todo'),
+    os.path.join(BASE_DIR, 'mainApp', 'static','mainApp'),
+    os.path.join(BASE_DIR, 'sendMail', 'static','sendMail'),
 ]
